df2aa13e3e7db177494f1f001c8962053c9f50c1
vasco <vastur@gmail.com>
2017-10-21 18:42:07 -0700

    Expose cjs module as main

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve';
 import commonJs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
 import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
+import { name, homepage, version } from './package.json';
 
 export default {
     input: 'src/index.js',
+    output: [
+        {
+            format: 'umd',
+            name: 'ForceGraph3D',
+            file: `dist/${name}.js`,
+            sourcemap: true
+        }
+    ],
     plugins: [
-        commonJs(),
-        nodeResolve(),
         postCss({
             plugins: [
                 postCssSimpleVars(),
                 postCssNested()
             ]
         }),
-        babel({
-            presets: [
-                ["es2015", { "modules": false }]
-            ],
-            plugins: ["external-helpers"],
-            babelrc: false
-        })
+        resolve(),
+        commonJs(),
+        babel({ exclude: 'node_modules/**' })
     ],
-    output: [
-        {
-            format: 'umd',
-            name: 'ForceGraph3D',
-            file: 'dist/3d-force-graph.js',
-            sourcemap: true
-        },
-        {
-            format: 'es',
-            file: 'dist/3d-force-graph.mjs'
-        }
-    ]
+    banner: `// Version ${version} ${name} - ${homepage}`
 };
---
+++ rollup.config.module.js
@@ -0,0 +1,29 @@
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
+import babel from 'rollup-plugin-babel';
+import { name, dependencies } from './package.json';
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            format: 'cjs',
+            file: `dist/${name}.common.js`
+        },
+        {
+            format: 'es',
+            file: `dist/${name}.mjs`
+        }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+        postCss({
+            plugins: [
+                postCssSimpleVars(),
+                postCssNested()
+            ]
+        }),
+        babel()
+    ]
+};
