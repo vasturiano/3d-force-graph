55c436fec736cdb133d42a5068c070630a258531
vasco <vastur@gmail.com>
2018-01-01 22:57:19 -0800

    Adjust indentation

--- rollup.config.module.js
+++ rollup.config.module.js
@@ -5,25 +5,25 @@ import babel from 'rollup-plugin-babel';
 import { name, dependencies } from './package.json';
 
 export default {
-    input: 'src/index.js',
-    output: [
-        {
-            format: 'cjs',
-            file: `dist/${name}.common.js`
-        },
-        {
-            format: 'es',
-            file: `dist/${name}.module.js`
-        }
-    ],
-    external: Object.keys(dependencies),
-    plugins: [
-        postCss({
-            plugins: [
-                postCssSimpleVars(),
-                postCssNested()
-            ]
-        }),
-        babel()
-    ]
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'cjs',
+      file: `dist/${name}.common.js`
+    },
+    {
+      format: 'es',
+      file: `dist/${name}.module.js`
+    }
+  ],
+  external: Object.keys(dependencies),
+  plugins: [
+    postCss({
+      plugins: [
+        postCssSimpleVars(),
+        postCssNested()
+      ]
+    }),
+    babel()
+  ]
 };
