7a16f0cdb29fe0dec4b7d00d2eb0532655846d39
vasco <vastur@gmail.com>
2019-10-22 23:34:42 -0700

    Add event objects to drag events

--- src/3d-force-graph.js
+++ src/3d-force-graph.js
@@ -250,7 +250,7 @@ export default Kapsule({
               .resetCountdown();  // prevent freeze while dragging
 
             node.__dragged = true;
-            state.onNodeDrag(node);
+            state.onNodeDrag(node, event);
           });
 
           dragControls.addEventListener('dragend', function (event) {
@@ -275,7 +275,7 @@ export default Kapsule({
 
               if (node.__dragged) {
                 delete(node.__dragged);
-                state.onNodeDragEnd(node);
+                state.onNodeDragEnd(node, event);
               }
             }
 
