51b022c3d37e5c60a8133787c0b9b1a209c4cf21
Adam Powers <apowers@ato.ms>
2024-03-02 00:26:06 -0800

    add WebXR support

--- src/3d-force-graph.js
+++ src/3d-force-graph.js
@@ -5,6 +5,7 @@ const three = window.THREE
   : { AmbientLight, DirectionalLight, Vector3 };
 
 import { DragControls as ThreeDragControls } from 'three/examples/jsm/controls/DragControls.js';
+import { XRButton as ThreeXRButton } from 'three/addons/webxr/XRButton.js';
 
 import ThreeForceGraph from 'three-forcegraph';
 import ThreeRenderObjects from 'three-render-objects';
@@ -124,6 +125,7 @@ export default Kapsule({
       triggerUpdate: false
     },
     enableNodeDrag: { default: true, triggerUpdate: false },
+    enableWebXR: { default: true, triggerUpdate: false },
     onNodeDrag: { default: () => {}, triggerUpdate: false },
     onNodeDragEnd: { default: () => {}, triggerUpdate: false },
     onNodeClick: { triggerUpdate: false },
@@ -413,6 +415,12 @@ export default Kapsule({
       });
 
     //
+
+    // config WebXR
+    if (state.enableWebXR) {
+      document.body.appendChild( ThreeXRButton.createButton( renderer ) );
+      renderer.xr.enabled = true;
+    }
 
     // Kick-off renderer
     this.renderer().setAnimationLoop(this._render);
